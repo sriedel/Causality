bf27aa83a1ff58444546c6f8dd67922db36e32fd
Sven Riedel <sr@gimp.org>
2010-02-13 15:41:48 +0100

    Minimal Causality::Event implementation

--- lib/causality/event.rb
+++ lib/causality/event.rb
@@ -1,6 +1,12 @@
 module Causality
   class Event
+    def self.collector
+      @collector ||= Causality::Collector.new
+    end
+     
     def self.cause( *args )
+      event = new #TODO pass args
+      self.collector.push event
     end
   end
 end
--- spec/lib/causality/event_spec.rb
+++ spec/lib/causality/event_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
 describe Causality::Event do
-  it "should have a Causality::Collector instance"
+  it "should have a class accessor to a collector instance" do
+    Causality::Event.should respond_to( :collector )
+  end
+
+  describe "the class collector accessor" do
+    it "should create a collector on first access" do
+      Causality::Collector.should_receive( :new ).once
+      Causality::Event.collector
+    end
+
+    it "should not create a new collector on a further calls" do
+      Causality::Collector.should_receive( :new ).once.
+                           and_return( mock( Causality::Collector ) )
+      c = Causality::Event.collector
+      Causality::Event.collector.should == c
+    end
+  end
+
+  describe "#initialize" do
+  end
 
   describe '.cause' do
-    it "should create an Event object"
-    it "should pass the Event object to Causality::Collector#push"
+    it "should pass a newly created Event object to Causality::Collector#push" do
+      Causality::Event.collector.should_receive( :push ).once.
+                                 with( instance_of( Causality::Event ) )
+      Causality::Event.cause
+    end
   end
 
   describe ".effect" do
