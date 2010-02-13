f019a705a3171b4c55eea0f30ab7a75c4cbf617f
Sven Riedel <sr@gimp.org>
2010-02-13 16:52:52 +0100

    Add spool storage to Causality::Collector

--- lib/causality/collector.rb
+++ lib/causality/collector.rb
@@ -1,11 +1,13 @@
 module Causality
   class Collector
     attr_reader :queue
+    attr_reader :spool
 
     RETRY_CONNECT_INTERVAL = 30
 
     def initialize( configfile = Causality::DEFAULT_CONFIG_FILE )
       @queue = Causality::QueueConnector::Starling.from_yml_file( configfile )
+      @spool = Causality::SpoolConnector.from_yml_file( configfile )
     end
 
     #FIXME: make queue name configurable
@@ -13,10 +15,21 @@ module Causality
       begin
         if @queue.up? || @queue.seconds_down >= RETRY_CONNECT_INTERVAL 
           @queue.set :causality_events, event
+        else
+          @spool.set :causality_events, event
         end
       rescue
-        #TODO: store in spool
+        @spool.set :causality_events, event
       end
+    end
+
+    private
+    def store_in_spool( value )
+      @spool.set :causality_events, value
+    end
+
+    def store_in_queue( value )
+      @queue.set :causality_events, value
     end
   end
 end
--- spec/lib/causality/collector_spec.rb
+++ spec/lib/causality/collector_spec.rb
@@ -5,9 +5,14 @@ describe Causality::Collector do
     @starling = mock( ::Starling )
     ::Starling.stub!( :new ).and_return( @starling )
 
+    @spool = mock( ::SpoolPool::Pool, :put => true )
+    ::SpoolPool::Pool.stub!( :new ).and_return( @spool )
 
     @starling_config = { :host => "some_host", :port => 31337 }
-    @default_config = YAML.dump( { :queue => @starling_config } )
+    @spool_config = { :spool_path => '/some/spool/path' }
+
+    @default_config = YAML.dump( { :queue => @starling_config,
+                                   :spool => @spool_config } )
     File.stub!( :read ).with( Causality::DEFAULT_CONFIG_FILE ).and_return( @default_config )
 
     @instance = Causality::Collector.new
@@ -18,7 +23,9 @@ describe Causality::Collector do
       before( :each ) do
         @other_config_file = "some_config_file"
         @other_starling_config = { :host => "other_host", :port => "73313" }
-        @other_config = YAML.dump( :queue => @other_starling_config )
+        @other_spool_config = { :spool_path => '/some/other/path' }
+        @other_config = YAML.dump( :queue => @other_starling_config,
+                                   :spool => @other_spool_config )
         File.stub!( :read ).with( @other_config_file ).and_return( @other_config )
       end
 
@@ -28,6 +35,13 @@ describe Causality::Collector do
                                             and_return( @starling )
         Causality::Collector.new( @other_config_file )
       end
+
+      it "should create a spool handle from the passed file" do
+        Causality::SpoolConnector.should_receive( :new ).once.
+                                  with( @other_spool_config ).
+                                  and_return( @spool )
+        Causality::Collector.new( @other_config_file )
+      end
     end
 
     context "no config file is passed" do
@@ -35,6 +49,13 @@ describe Causality::Collector do
         Causality::QueueConnector::Starling.should_receive( :new ).once.
                                             with( @starling_config ).
                                             and_return( @starling )
+        Causality::Collector.new
+      end
+
+      it "should create a spool handle from the default configuration file" do
+        Causality::SpoolConnector.should_receive( :new ).once.
+                                  with( @spool_config ).
+                                  and_return( @spool )
         Causality::Collector.new
       end
     end
@@ -61,7 +82,10 @@ describe Causality::Collector do
         @starling.stub!( :set ).and_raise( Causality::QueueUnavailable )
       end
 
-      it "should store the event in the spool instead"
+      it "should store the event in the spool instead" do
+        @spool.should_receive( :put ).with( :causality_events, @event )
+        @instance.push @event
+      end
     end
 
     context "the queue is down" do
@@ -86,8 +110,22 @@ describe Causality::Collector do
           before( :each ) do
             @starling.stub!( :set ).and_return( true )
           end
-          it "should not store the event in the queue"
-          #FIXME: how to check that it's passing messages to starling again?
+
+          it "should not store the event in the spool" do
+            @spool.should_not_receive( :put )
+            @instance.push @event
+          end
+        end
+
+        context "the queue reconnection didn't succeed" do
+          before( :each ) do
+            @starling.stub!( :set ).and_raise( Causality::QueueUnavailable )
+          end
+
+          it "should store the event in the spool" do
+            @spool.should_receive( :put ).with( :causality_events, @event )
+            @instance.push @event
+          end
         end
       end
 
@@ -97,7 +135,11 @@ describe Causality::Collector do
           @instance.queue.stub!( :seconds_down ).and_return( seconds )
         end
 
-        it "should store the event in the spool"
+        it "should store the event in the spool" do
+          @spool.should_receive( :put ).with( :causality_events, @event )
+          @instance.push @event
+        end
+
         it "should not try to store the event in the queue" do
           @starling.should_not_receive( :set )
           @instance.push( @event )
