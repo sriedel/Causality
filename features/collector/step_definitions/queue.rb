146ac56bd3089df1c86896dfebecd217627ad83d
Sven Riedel <sr@gimp.org>
2010-02-13 15:52:38 +0100

    Adjust cucumber test to real implementation

--- features/collector/step_definitions/queue.rb
+++ features/collector/step_definitions/queue.rb
@@ -1,10 +1,11 @@
 Given /^the Queue is up$/ do
   @starling_instance = mock( Starling )
+  @starling_instance.stub!( :set ).and_return { |queue, value| @to_queue, @got_value = queue, value }
+
   Starling.stub!( :new ).and_return( @starling_instance )
 end
 
 Then /^the Event should be stored in the Queue$/ do
-  @starling_instance.should_receive( :set ).
-                     with( :events, instance_of( Causality::Event ) ).
-                     and_return( true )
+  @to_queue.should == :causality_events
+  @got_value.should be_a( Causality::Event )
 end
