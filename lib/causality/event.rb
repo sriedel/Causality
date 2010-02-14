7e2d7cdfc517bab60e1f5985f6c884f3541b3d9f
Sven Riedel <sr@gimp.org>
2010-02-14 21:05:43 +0100

    Add attribute: Causality::Event#name

--- lib/causality/event.rb
+++ lib/causality/event.rb
@@ -1,12 +1,18 @@
 module Causality
   class Event
+    attr_reader :name
+
     def self.collector
       @collector ||= Causality::Collector.new
     end
      
     def self.cause( *args )
-      event = new #TODO pass args
+      event = new( *args )
       self.collector.push event
+    end
+
+    def initialize( name )
+      @name = name
     end
   end
 end
--- spec/lib/causality/event_spec.rb
+++ spec/lib/causality/event_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 describe Causality::Event do
+  before( :each ) do
+    @name = :my_event
+    @instance = Causality::Event.new( @name )
+  end
+
   it "should have a class accessor to a collector instance" do
     Causality::Event.should respond_to( :collector )
+  end
+
+  it "should have a name attribute" do
+    @instance.should respond_to( :name ) 
   end
 
   describe "the class collector accessor" do
@@ -19,17 +28,21 @@ describe Causality::Event do
     end
   end
 
-  describe "#initialize" do
-  end
-
   describe '.cause' do
     it "should pass a newly created Event object to Causality::Collector#push" do
       Causality::Event.collector.should_receive( :push ).once.
                                  with( instance_of( Causality::Event ) )
-      Causality::Event.cause
+      Causality::Event.cause( :my_event )
     end
   end
 
   describe ".effect" do
   end
+
+  describe "#initialize" do
+    it "should set the name" do
+      @instance.name.should == @name
+    end
+  end
+
 end
