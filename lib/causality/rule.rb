83ec8415757e0ef3c026f24f4f69efb02f21f54e
Sven Riedel <sr@gimp.org>
2010-02-14 08:38:22 +0100

    Initial fleshed out Causality::Rule

--- lib/causality/rule.rb
+++ lib/causality/rule.rb
@@ -1,4 +1,19 @@
 module Causality
   class Rule
+    attr_reader :name
+    attr_reader :activating_causes
+    attr_reader :resulting_effects
+    attr_reader :rule
+
+    def initialize( name, activating_causes, resulting_effects, &rule )
+      @name = name
+      @activating_causes = activating_causes
+      @resulting_effects = resulting_effects
+      @rule = rule
+    end
+
+    def evaluate( event )
+      @rule.call( event ) rescue false
+    end
   end
 end
--- spec/lib/causality/rule_spec.rb
+++ spec/lib/causality/rule_spec.rb
@@ -1,21 +1,76 @@
 require 'spec_helper'
 
 describe Causality::Rule do
-  it "should have a name attribute"
-  it "should have an activating_causes attribute"
-  it "should have an resulting_effects attribute"
+  before( :each ) do
+    @name = "my test rule"
+    @activating_causes = [ :foo, :man, :chu ]
+    @resulting_effects = [ :gravity_wave, :universe_implosion ]
+    @always_rule = Proc.new { |event| true }
+    @never_rule = Proc.new { |event| false }
+    @exceptional_rule = Proc.new{ |event| raise }
+    @instance = Causality::Rule.new( @name, @activating_causes, @resulting_effects, &@always_rule )
+  end
+
+  it "should have a name attribute" do
+    @instance.should respond_to( :name )
+  end
+
+  it "should have an activating_causes attribute" do
+    @instance.should respond_to( :activating_causes )
+  end
+
+  it "should have an resulting_effects attribute" do
+    @instance.should respond_to( :resulting_effects )
+  end
+
+  it "should have a rule attribute" do
+    @instance.should respond_to( :rule )
+  end
 
   describe "#initialize" do
-    it "should set the name"
-    it "should set the rule proc from the block"
-    it "should set the activating_causes"
-    it "should set the resulting_effects"
+    it "should set the name" do
+      @instance.name.should == @name
+    end
+
+    it "should set the rule proc from the block" do
+      @instance.rule.should == @always_rule
+    end
+
+    it "should set the activating_causes" do
+      @instance.activating_causes.should == @activating_causes
+    end
+
+    it "should set the resulting_effects" do
+      @instance.resulting_effects.should == @resulting_effects
+    end
   end
 
   describe "#evaluate" do
-    it "should execute the proc with the passed event"
-    it "should return true if the proc evaluates to true"
-    it "should return false if the proc doesn't evaluate to true"
-    it "should return false if the proc raises an exception"
+    before( :each ) do
+      @event = :some_event
+    end
+
+    it "should execute the proc with the passed event" do
+      @always_rule.should_receive( :call ).with( @event ).once
+      @instance.evaluate( @event )
+    end
+
+    it "should return true if the proc evaluates to true" do
+      Causality::Rule.new( @name, @activating_causes, @resulting_effects, &@always_rule ).
+                      evaluate( @event ).
+                      should be_true
+    end
+
+    it "should return false if the proc doesn't evaluate to true" do
+      Causality::Rule.new( @name, @activating_causes, @resulting_effects, &@never_rule ).
+                      evaluate( @event ).
+                      should be_false
+    end
+
+    it "should return false if the proc raises an exception" do
+      Causality::Rule.new( @name, @activating_causes, @resulting_effects, &@exceptional_rule ).
+                      evaluate( @event ).
+                      should be_false
+    end
   end
 end
