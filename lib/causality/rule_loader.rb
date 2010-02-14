f6ffafae71e595d7217188b8e90bd9ce1f833e36
Sven Riedel <sr@gimp.org>
2010-02-14 18:26:44 +0100

    Refactor RuleLoader

--- lib/causality/rule_loader.rb
+++ lib/causality/rule_loader.rb
@@ -3,18 +3,14 @@ module Causality
     RULE_FILE_GLOB = "*_rules.rb"
 
     def self.load_directory( directory )
-      rules = []
-      
-      rule_files_in_directory( directory ).each do |rule_file|
-        next unless File.file? rule_file
-        begin
-          files_rule_array = extract_rules_array( rule_file )
-          rules += create_rules( files_rule_array )
-        rescue
-        end
-        
-      end
-      rules 
+      rule_files_in_directory( directory ).select{ |f| File.file? f }.  
+                                           collect { |file| load_file file }.
+                                           flatten.
+                                           uniq
+    end
+
+    def self.load_file( file )
+      create_rules( extract_rules_array( file ) ) rescue []
     end
 
     private
@@ -27,14 +23,11 @@ module Causality
     end
 
     def self.create_rules( array )
-      rules = []
-      array.each do |file_rule|
-        begin
-          rules << Causality::Rule.from_hash( file_rule )
-        rescue
-        end
-      end
-      rules
+      array.collect { |file_rule| create_rule( file_rule ) }.compact
+    end
+
+    def self.create_rule( hash )
+      Causality::Rule.from_hash( hash ) rescue nil
     end
 
   end
--- spec/lib/causality/rule_loader_spec.rb
+++ spec/lib/causality/rule_loader_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe Causality::RuleLoader do
-  describe ".load_directory" do
-    before( :each ) do
-      @test_rule_path = Pathname.new( 'test_rules' )
-      @test_rule_file = Pathname.new( 'my_rules.rb' )
-      @other_rule_file = Pathname.new( 'other_rules.rb' )
-      @loading_exception_rule_file = Pathname.new( 'loading_exception_rules.rb' )
-    end
+  before( :each ) do
+    @test_rule_path = Pathname.new( 'test_rules' )
+    @test_rule_file = @test_rule_path + 'my_rules.rb' 
+    @other_rule_file = @test_rule_path + 'other_rules.rb' 
+    @loading_exception_rule_file = @test_rule_path + 'loading_exception_rules.rb' 
+  end
 
+  describe ".load_directory" do
     it "should load the rules in each rule file in the given directory" do
       array = Causality::RuleLoader.load_directory( @test_rule_path.to_s )
       array.size.should == 5
@@ -24,6 +24,25 @@ describe Causality::RuleLoader do
     end
 
     it "should skip rules that cause an exception while creating a Causality::Rule object" 
+  end
+
+  describe ".load_file" do
+    it "should load the rules in each rule file in the given directory" do
+      Causality::RuleLoader.load_file( @test_rule_file.to_s ).size.should == 2
+    end
+
+    it "should return an array of Causality::Rule objects" do
+      Causality::RuleLoader.load_file( @test_rule_file.to_s ).each do |rule|
+        rule.should be_a( Causality::Rule ) 
+      end
+    end
+
+    it "should skip files that cause an exception while loading" do
+      lambda { Causality::RuleLoader.load_file( @test_rule_file.to_s ) }.should_not raise_error
+    end
+
+    it "should skip rules that cause an exception while creating a Causality::Rule object" 
+
   end
 
 end
